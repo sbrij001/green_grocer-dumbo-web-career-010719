f35cf76e865d63dc0de2ea8ca0f7e28155a09921
github username <github email address>
2019-01-09 11:57:48 -0500

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,20 @@
+# check if its nil and assign
+# return new hash
+require"pry"
 def consolidate_cart(cart)
-  # code here
+  new_hash= {}
+  count = 1
+  cart.each do |groceries|
+    groceries.each do |item,data|
+      new_hash[item] = data
+      if new_hash[item][:count] == nil
+        new_hash[item][:count] = count
+      else
+        new_hash[item][:count] = count + 1
+      end
+    end
+  end
+  new_hash
 end
 
 def apply_coupons(cart, coupons)
